fc6ba45d5d2abdef4cab0ab8f0e04ddc906b4763
z0052mwa <rutikutekar2003@gmail.com>
2025-04-08 10:16:55 +0530

    Revert "Get Req commit"
    This reverts commit 3669396a0058e941af2ea8b160ca5d172be60f3e.

--- PostmanCloneUI/Form1.cs
+++ PostmanCloneUI/Form1.cs
@@ -1,69 +1,15 @@
-using System.Windows.Forms.Design;
-using System.Net.Http;
-
-//using PostmanCloneLibrary;
-
 namespace PostmanCloneUI
-
 {
-
-   
     public partial class Form1 : Form
     {
-
-        private readonly IApiAccess api = new ApiAccess();
-
         public Form1()
         {
             InitializeComponent();
-            //api = new ApiService(); 
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
 
         }
-
-        private void textBox2_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private async void callApi_Click(object sender, EventArgs e)
-        {
-            // validtaionn requires
-
-            if (api.IsValidUrl(apiText.Text)== false) {
-                systemStatus.Text = "Invalid URL..";
-                return;
-            }
-
-            try
-            {
-                systemStatus.Text = "API calling...";
-
-                resultsText.Text=  await api.CallApiAsync(apiText.Text);
-
-                systemStatus.Text = "Ready";
-
-
-
-            }
-            catch (Exception ex)
-            {
-
-                resultsText.Text = "Error coming as:" + ex.Message;
-                //systemStatus.Text = "Error";
-            }
-
-
-        }
-
-        private void apiText_TextChanged(object sender, EventArgs e)
-        {
-
-        }
     }
-
-   
 }
